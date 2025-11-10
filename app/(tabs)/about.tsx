709d52e21fb5a641d963785477e96c79be8aa26b
aRav3n <24660772+aRav3n@users.noreply.github.com>
2025-11-10 12:35:24 +0530

    build: update "about" tab to include github URL

--- app/(tabs)/about.tsx
+++ app/(tabs)/about.tsx
@@ -1,9 +1,12 @@
-import { StyleSheet, Text, View } from "react-native";
+import { Linking, StyleSheet, Text, TouchableOpacity, View } from "react-native";
 
 export default function AboutScreen() {
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>Super cute!</Text>
+      <Text style={styles.text}>You can find the source code here:</Text>
+      <Text onPress={() => {
+        Linking.openURL("https://github.com/aRav3n/gin-rummy-app")
+      }} style={styles.text}>https://github.com/aRav3n/gin-rummy-app</Text>
     </View>
   );
 }
